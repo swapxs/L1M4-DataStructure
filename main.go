870c70d4f924f7b874e32a7f981d42a9176884a6
swapxs <swapnil.bhowmik@xenonstack.com>
2025-01-28 09:49:50 +0530

    init commit with basic code

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+func sol(cap1, cap2, t int) { }
+
+func main() {
+	bigJugCapacity := 4
+	smallJugCapacity := 3
+	bigJugTarget := 2
+
+	sol(bigJugCapacity, smallJugCapacity, bigJugTarget)
+}
