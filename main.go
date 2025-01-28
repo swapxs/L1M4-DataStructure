9db4adbbb551cf6f521990e45f4f7f2335431e7d
swapxs <swapnil.bhowmik@xenonstack.com>
2025-01-28 10:29:48 +0530

    Algorithm written for context

--- main.go
+++ main.go
@@ -1,4 +1,22 @@
+/*
+* Solution algorithm:
+* Step 1: Empty all the Jugs
+* Step 2: Fill the smaller Jug(3)
+* Step 3: Transfer water from Jug(3) to Jug(4), making
+*         Jug(3) -> 0 and Jug(4) -> 3 (with 1 empty space)
+* Step 4: Fill Jug(3)
+* Step 5: Transfer water from Jug(3) to Jug(4), making
+* 		  Jug(3) -> 2 and Jug(4) -> 3 + 1 (1 unit is filled
+* 		  since 3 already exists)
+* Step 6: Empty the Jug(4)
+* Step 7: Transfer the remaining 2 units of water Jug(3) -> Jug(4)
+* */
+
 package main
+
+import (
+	"fmt"
+)
 
 func sol(cap1, cap2, t int) { }
 
